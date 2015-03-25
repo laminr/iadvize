afec29110c70931119caaa5ac4e396eae69eeda1
Tweety <tdelambilly@gmail.com>
2015-03-25 21:00:46 +0100

    validating unitary test

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,21 +2,14 @@
 
 namespace AppBundle\Tests\Controller;
 
+use AppBundle\Business\VdmBusiness;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use AppBundle\Entity\Vdm;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
-    {
-        $client = static::createClient();
 
-        $crawler = $client->request('GET', '/app/example');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
-    }
-
-    public function getVdmScenario()
+    public function testVdmScenarioTest()
     {
     	$urlHome = "http://www.viedemerde.fr/";
         $client = static::createClient();
@@ -27,23 +20,21 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $crawler->filter('.wrapper div.post.article');
         foreach ($crawler as $node) {
-
             $vdm = VdmBusiness::parseOneVdm($node);
-            $this->assertTrue(trim($vdm->getText()) != "");
+            $this->assertTrue(trim($vdm->getContent()) != "");
         }
     }
 
-    public function getApiScenario()
+    public function testApiScenarioTest()
     {
-
     	$client = static::createClient();
         $client->request('GET', '/api/posts');
 
 		$response = $client->getResponse();
-		
+
         $this->assertEquals(200, $response->getStatusCode());
 	    $this->assertSame('application/json', $response->headers->get('Content-Type'));
-	    $this->assertNotEmpty($client->getResponse()->getContent()); 
+	    $this->assertNotEmpty($client->getResponse()->getContent());
 
     }
 }
