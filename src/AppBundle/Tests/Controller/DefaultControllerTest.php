a81c00663085e8ad826436f771ac3b4a0da7bb31
Thibault <thibault@revisersavoiture.com>
2015-03-25 09:27:51 +0100

    first unitary test

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -15,4 +15,35 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
     }
+
+    public function getVdmScenario()
+    {
+    	$urlHome = "http://www.viedemerde.fr/";
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $urlHome);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $crawler = $crawler->filter('.wrapper div.post.article');
+        foreach ($crawler as $node) {
+
+            $vdm = VdmBusiness::parseOneVdm($node);
+            $this->assertTrue(trim($vdm->getText()) != "");
+        }
+    }
+
+    public function getApiScenario()
+    {
+
+    	$client = static::createClient();
+        $client->request('GET', '/api/posts');
+
+		$response = $client->getResponse();
+		
+        $this->assertEquals(200, $response->getStatusCode());
+	    $this->assertSame('application/json', $response->headers->get('Content-Type'));
+	    $this->assertNotEmpty($client->getResponse()->getContent()); 
+
+    }
 }
