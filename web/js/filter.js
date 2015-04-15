29a42ac4ef261b118e0baa7e0d1da8e40b1b435c
Tweety <tdelambilly@gmail.com>
2015-04-15 20:57:12 +0200

    correcting date filter

--- web/js/filter.js
+++ web/js/filter.js
@@ -4,4 +4,15 @@ angular.module('iAdvizeFilters', [])
             return String(text).replace(/\s/mg, "");
         };
     })
+    .filter('dateFr', function($filter) {
+        return function(input) {
+            if(input == null){ return ""; }
+
+            var _date = $filter('date')(new Date(input), 'dd-MM-yyyy HH:mm');
+
+            return _date.toUpperCase();
+
+        };
+    });
+
 ;
